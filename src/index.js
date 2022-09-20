78b69688b289aef2e2e324920c120649b281c4e9
Inna <doordie9893"gmail.com>
2022-09-20 23:55:55 +0200

    changed state to store start with some oop

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import reportWebVitals from './reportWebVitals';
-import {rerenderTree} from "./render";
-import state from "./components/redux/state";
+import store from "./components/redux/state";
+import ReactDOM from 'react-dom/client';
+import App from "./App";
 
 
 /*
@@ -22,12 +23,28 @@ const db = mysql.createConnection({
 app.listen(3001, () => {
     console.log("running server");
 });
+
 */
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let rerenderTree = (state) => {
 
+    root.render(
+        <React.StrictMode>
+            <App state={state}
+                 addWordToVocabulary={store.addWordToVocabulary.bind(store)} // bind lets to connect method with store and not let to be called with props for another component
+                 updateNewWordText={store.updateNewWordText.bind(store)}
+                 newWordText={store.getState().newWordText}
+                 newTranslationText={store.getState().newTranslationText}/>
+        </React.StrictMode>
+    );
 
+}
 
-rerenderTree(state);
+rerenderTree(store.getState());
 
+//imported subscribe from state.js and called rerenderTree in it -> avoid loop between index.js and state.js
+
+store.subscribe(rerenderTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
