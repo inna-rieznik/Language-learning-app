382f2c4aca7b54d4fafbebe99c512ed6630a12d7
Inna <doordie9893"gmail.com>
2022-10-03 22:23:26 +0200

    make separate foute for register and lincked it to index.js

--- frontend/src/components/mainPage/lessons/CreateLesson.jsx
+++ frontend/src/components/mainPage/lessons/CreateLesson.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Box from "@mui/material/Box";
 import {TextField} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import Button from "@mui/material/Button";
