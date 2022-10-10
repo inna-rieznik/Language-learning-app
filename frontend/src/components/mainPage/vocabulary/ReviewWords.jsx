079cbdbda914e9fdc213e10878e70e17b2fa18ba
Inna <doordie9893"gmail.com>
2022-10-10 23:03:46 +0200

    created template for quiz

--- frontend/src/components/mainPage/vocabulary/ReviewWords.jsx
+++ frontend/src/components/mainPage/vocabulary/ReviewWords.jsx
@@ -5,32 +5,32 @@ import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
 
-import Action from "../action/Action";
+import Action from "./action/Action";
 
-const ReviewGrammar = (props) => {
+const ReviewWords = (props) => {
 
     //Settings component
     return (
         <div className={s.content}>
-            <Breadcrumbs aria-label="breadcrumb">
-                <Link
-                    underline="hover"
-                    color="inherit"
-                    href="/"
-                >
-                    Home
-                </Link>
-                <Typography color="text.primary">Review Grammar</Typography>
-            </Breadcrumbs>
-            <h1>Review Grammar</h1>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                        href="/"
+                    >
+                        Home
+                    </Link>
+                    <Typography color="text.primary">ReviewWords</Typography>
+                </Breadcrumbs>
+            <h1>ReviewWords</h1>
             <Action urlName='quiz' title='Quiz'/>
             <Action urlName='flashcards' title='Flashcards'/>
             <Action urlName='review_words/matching_translation' title='Matching: connect word with translation'/>
             <Action urlName='review_words/matching_sentences' title='Complete the sentence'/>
-            <Action urlName='review_words/fill_the_gap' title='Fill the gap'/>
+
 
         </div>
     );
 }
 
-export default ReviewGrammar;
+export default ReviewWords;
