675d0d1cdd636cbb93e50b18513422eaebf75ac9
Inna <doordie9893"gmail.com>
2022-11-05 21:47:59 +0100

    almost finished quiz game -> it count points for correct answe

--- frontend/src/components/mainPage/vocabulary/quiz/Result.jsx
+++ frontend/src/components/mainPage/vocabulary/quiz/Result.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add";
 import s from "./QuizPage.module.css";
-import {questions} from "./QuizGame";
 
 
-
-const Result = ({correct}) => {
+const Result = ({countCorrect}) => {
     return (
-    <div className={s.quiz}>
-        <div className={s.modal}>
-            <h2>You answer {correct} of {questions.length} questions</h2>
-                <Button variant="contained">Try again</Button> {/*here will be TRY AGAIN OR NEXT v zavislosti na tom, kde je vyuzita komponenta*/}
-
+        <div>
+            <h2>You answer {countCorrect} of 4 questions</h2>
+            <Button variant="contained">
+                Try again</Button>
+            {/*here will be TRY AGAIN OR NEXT v zavislosti na tom, kde je vyuzita komponenta*/}
         </div>
-    </div>
     );
 }
 
